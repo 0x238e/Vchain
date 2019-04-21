6558aa51e7fc560f393a9cee46ff4efe9816d0e2
victoryang00 <yangyiwei2000@gmail.com>
2019-04-21 15:49:38 +0800

    fix

--- client/new1/run.py
+++ client/new1/run.py
@@ -16,7 +16,7 @@ def update(cv):
     level=["is-primary","is-danger","is-info","is-warning"][random.randint(0,3)]
     name=["surpass","compete","opposite"][random.randint(0,2)]
     decider=["human","car"][random.randint(0,1)]
-    status=["","accept","reject"][random.randint(0,2)]
+    status=""
     userlevel=str(random.randint(0,10))
     price=str(random.randint(0,500))
 
@@ -48,23 +48,12 @@ def update(cv):
     }]
 
     event=json.dumps(event)
+    r=requests.post("http://api.v.noinfinity.top/event",event)
     contract.update(hashlib.md5(event.encode()).hexdigest(),user1)
 
-    r=requests.post("http://api.v.noinfinity.top/event",event)
-
+    
     time.sleep(3)
-
-    level=["is-primary","is-danger","is-info","is-warning"][random.randint(0,3)]
-    name=["surpass","compete","opposite"][random.randint(0,2)]
-    decider=["human","car"][random.randint(0,1)]
-    status=["","accept","reject"][random.randint(0,2)]
-    userlevel=str(random.randint(0,10))
-    price=str(random.randint(0,500))
-
-    gps="GPRMC,080655.00,A,4546.40891,N,12639.65641,E,1.045,328.42,170809,,,A*60"
-    gps="GPRMC,{},A,{},N,{},E,1.045,328.42,170809,,,A*60".format(int(time.time()),4546.40891+5*time.time(),12639.65641+5*time.time())
-    gps=[random.random()*100,random.random()*100]
-    sensorcv=cv
+    status=["accept","reject"][random.randint(0,1)]
 
     event=[{
         "type":"update",
@@ -80,15 +69,16 @@ def update(cv):
             "expire":exp,
             "decider":decider,
             "status":status,
-            "userlevel":userlevel
-        },
-        "data":{
-            "gps":gps,
-            "sensorcv":sensorcv
+            "userlevel":userlevel,
+        
+            "data":{
+                "gps":gps,
+                "sensorcv":sensorcv
+            }
         }
     }]
 
     event=json.dumps(event)
+    r=requests.post("http://api.v.noinfinity.top/event",event)
     contract.update(hashlib.md5(event.encode()).hexdigest(),user1)
 
-    r=requests.post("http://api.v.noinfinity.top/event",event)
