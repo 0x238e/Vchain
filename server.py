313c5c90059287223c6759ade179c9d0ada4ae2e
ZarkLngeW <zhanglw2@shanghaitech.edu.cn>
2019-04-21 08:52:09 +0800

    updated png

---
+++ server.py
@@ -0,0 +1,26 @@
+from socket import *
+from time import ctime
+
+host = ''
+port = 12345
+buffsize = 2048
+ADDR = (host,port)
+
+tctime = socket(AF_INET,SOCK_STREAM)
+tctime.bind(ADDR)
+tctime.listen(3)
+
+while True:
+    print('Wait for connection ...')
+    tctimeClient,addr = tctime.accept()
+    print("Connection from :",addr)
+
+    while True:
+        data = tctimeClient.recv(buffsize).decode()
+        if not data:
+            # break
+            pass
+        print(data)
+        
+    tctimeClient.close()
+tctimeClient.close()
